d699416d74830760941f2434cc3c1b4b0ccfc41a
Sigma <mzd2313@mail.ru>
2025-05-21 16:26:41 +0300

    Добавлена ER-диаграмма в нотации Чена v1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Нажми меня");
+        btn.setOnAction(e -> System.out.println("Кнопка нажата!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Пример JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
